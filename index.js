e570dcd47b759be30d1f535750582d0cfb48a193
putraaxzy <wibowo.yunanto41@smk.belajar.id>
2024-03-02 07:08:12 +0700

    fix error 'await' `tsconfig.json` dengan ini tidak perlu menggunakan npx esrun lagi.

--- index.js
+++ index.js
@@ -1,45 +1,82 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-var answers = await inquirer_1.default.prompt([
-  {
-    type: "number",
-    name: "numberOne",
-    message: "Masukan Angka Pertama: ",
-  },
-  {
-    type: "number",
-    name: "numberTwo",
-    message: "Masukan Angka Kedua: ",
-  },
-  {
-    type: "list",
-    name: "operator",
-    choices: ["*", "+", "-", "/"],
-    message: "Pilih Operator: ",
-  },
+import inquirer from "inquirer";
+import { createConnection } from "mysql";
+const pool = createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "kalku",
+});
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Masukan Angka Pertama: ",
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Masukan Angka Kedua: ",
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "Pilih Operator: ",
+    },
 ]);
 // menampilkan output hasil
-var numberOne = answers.numberOne,
-  numberTwo = answers.numberTwo,
-  operator = answers.operator;
+const { numberOne, numberTwo, operator, hasil } = answers;
 if (numberOne && numberTwo && operator) {
-  var result = 0;
-  if (operator === "+") {
-    result = numberOne + numberTwo;
-  } else if (operator === "-") {
-    result = numberOne - numberTwo;
-  } else if (operator === "/") {
-    if (numberTwo !== 0) {
-      result = numberOne / numberTwo;
-    } else {
-      console.error("Error: Division by zero");
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+        const data = {
+            angka1: numberOne,
+            angka2: numberTwo,
+            operator: operator,
+            hasil: result,
+        };
+        pool.query("insert into kalku_ts set ?", data);
     }
-  } else if (operator === "*") {
-    result = numberOne * numberTwo;
-  } else {
-    console.error("Error: Invalid operator");
-  }
-  // menampilkan hasil atau result
-  console.log("Hasil kamu adalah: ", result);
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+        const data = {
+            angka1: numberOne,
+            angka2: numberTwo,
+            operator: operator,
+            hasil: result,
+        };
+        pool.query("insert into kalku_ts set ?", data);
+    }
+    else if (operator === "/") {
+        if (numberTwo !== 0) {
+            result = numberOne / numberTwo;
+            const data = {
+                angka1: numberOne,
+                angka2: numberTwo,
+                operator: operator,
+                hasil: result,
+            };
+            pool.query("insert into kalku_ts set ?", data);
+        }
+        else {
+            console.error("Error: Division by zero");
+        }
+    }
+    else if (operator === "*") {
+        result = numberOne * numberTwo;
+        const data = {
+            angka1: numberOne,
+            angka2: numberTwo,
+            operator: operator,
+            hasil: result,
+        };
+        pool.query("insert into kalku_ts set ?", data);
+    }
+    else {
+        console.error("Error: Invalid operator");
+    }
+    // menampilkan hasil atau result
+    console.log("Hasil kamu adalah: ", result);
+    pool.end();
 }
